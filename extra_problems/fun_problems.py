1ad082a39e6f8745552fef0b29e3e2846ad181f6
jwc20 <choij2015@gmail.com>
2022-08-01 17:35:04 -0700

    added fun problems

---
+++ extra_problems/fun_problems.py
@@ -0,0 +1,41 @@
+"""
+Question 1
+Given an integer array of length N and an int t , find all good tuples.
+A good tuple is defined as A[i] < A[j] < A[k] and A[i] + A[j] + A[k] <= t. 
+Return the number of tuples
+
+Question 2
+Given an array of non negative integers,length n, find all pairs (i,j) where A[i] + rev(A[j]) = rev(A[i]) + A[j].
+The rev() function is the reverse of the integer i.e rev(456) = 654.
+Return the number of pairs
+
+Question 3
+Given an integer n, find the sum n + rev(n). 
+The rev() function reverses the integer but also keeps track of the beginning zero
+i.e rev(4200) = 2400 , rev(301) = 103. 
+Return the sum
+
+Question 4
+Given a 2D array of integers and an int k, 
+find the k x k sub-matrix that has the max sum and 
+return the sum of all the unique integers in the sub-matrix.
+If there are more than one sub-matrices with the max sum their values should be included in the result.
+"""
+
+from typing import List
+
+def q1(A: List[int]) -> int:
+    return -1 
+
+def q2(A: List[int]) -> int:
+    return -1 
+
+def q3(n: int) -> int:
+    return -1
+
+def q4(A: List[List[int]], k: int) -> int:
+    return -1
+
+
+
+
