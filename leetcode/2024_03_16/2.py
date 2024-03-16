4eb8e0613bfa47d30c44b88d15e671b9bb6d3417
jwc20 <choij2015@gmail.com>
2024-03-16 14:16:54 +0900

    LC 2270. reduce space complexity

--- leetcode/2024_03_16/2.py
+++ leetcode/2024_03_16/2.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def left_section_is_greater_than_right_section(nums):
     prefix = [nums[0]]
 
@@ -16,9 +19,24 @@ def left_section_is_greater_than_right_section(nums):
     return ans
 
 
+def waysToSplitArray(nums: List[int]) -> int:
+    ans = left_section = 0
+    total = sum(nums)
+
+    for i in range(len(nums) - 1):
+        left_section += nums[i]
+        right_section = total - left_section
+        if left_section >= right_section:
+            ans += 1
+
+    return ans
+
+
 if __name__ == "__main__":
     nums = [2, 3, 1, 5, 2, 1, 4]
     print(left_section_is_greater_than_right_section(nums))
+    print(waysToSplitArray(nums))
 
     nums1 = [10, 4, -8, 7]
     print(left_section_is_greater_than_right_section(nums1))
+    print(waysToSplitArray(nums1))
